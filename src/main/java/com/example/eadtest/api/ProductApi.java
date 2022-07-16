22027c8147ec3d921e149adb6b50854be07055eb
DELL <duongquocv@gmail.com>
2022-07-16 15:42:35 +0700

    update

---
+++ src/main/java/com/example/eadtest/api/ProductApi.java
@@ -0,0 +1,22 @@
+package com.example.eadtest.api;
+
+import com.example.eadtest.entity.Product;
+import com.example.eadtest.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/findAll")
+public class ProductApi {
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Product> findAll(){
+        return productService.findAll();
+    }
+}
---
+++ src/main/java/com/example/eadtest/service/SaleService.java
@@ -0,0 +1,18 @@
+package com.example.eadtest.service;
+
+import com.example.eadtest.entity.Sale;
+import com.example.eadtest.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+    @Autowired
+    SaleRepository saleRepository;
+
+    public List<Sale> findAll() {
+        return saleRepository.findAll();
+    }
+}
